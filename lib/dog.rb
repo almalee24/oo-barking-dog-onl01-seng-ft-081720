91d19dfbb82f646923dce335190dd8845ee379d1
Alma Flores <flores.almalee@gmail.com>
2020-08-24 19:11:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
 # Your code goes here!
 class Dog 
-  def 
+  def name(dogs_name)
+    @this_dogs_name = dogs_name
+  end 
+  
+  def name
+    @this_dogs_name
+  end 
+  
+  def bark 
+    puts "woof!"
+  end
 end
